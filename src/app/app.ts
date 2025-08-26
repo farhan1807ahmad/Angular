26341c07b1ef0f4dfcc2631f288e2df501e9da2f
farhan1807ahmad <fa9711173@gmail.com>
2025-08-26 20:44:20 +0530

    component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,19 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// import { RouterOutlet } from '@angular/router';
+import { Login } from './login/login';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Login],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = 'Angular-Intro';
+  name = "Farhan Ahmad";
+  x = 10;
+  y = 20;
+
+  user1="farhan";
+  user2="ahmad";
 }
