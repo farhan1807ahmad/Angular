8a20c23b75490ccb7718b8a681c9547a7ae2c0aa
farhan1807ahmad <fa9711173@gmail.com>
2025-08-27 21:04:30 +0530

    function and dt

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,19 +1,37 @@
 import { Component, signal } from '@angular/core';
 // import { RouterOutlet } from '@angular/router';
-import { Login } from './login/login';
+// import { Login } from './login/login';
 
 @Component({
   selector: 'app-root',
-  imports: [Login],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = 'Angular-Intro';
-  name = "Farhan Ahmad";
-  x = 10;
-  y = 20;
+  // protected readonly title = 'Angular-Intro';
+  // name = "Farhan Ahmad";
+  // x = 10;
+  // y = 20;
 
-  user1="farhan";
-  user2="ahmad";
+  // user1="farhan";
+  // user2="ahmad";
+  
+  // handleClickEvent(){
+  //   console.log("Function Called");
+  //   this.otherFunction(); 
+  // }
+  // otherFunction(){
+  //   console.log("Other Function Called");
+  // }
+
+  // name:string="Farhan Ahmad";
+
+  // updateName(){
+  //   this.name="Angular Developer";
+  // }
+
+  sum(a:number,b:number){
+    console.log(a+b);
+  }
 }
